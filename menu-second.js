0ca27a2b7c01ed81812c609ffd1ca90bc4dd5933
borisgra <boris.gra.dev@gmail.com>
2025-04-13 12:15:54 +0300

    second Jsonplaceholder

--- menu-second.js
+++ menu-second.js
@@ -1,7 +1,5 @@
 export function menuItemsData(newGreed)  {
     const defColor = {color: '#0033cc',bgcolor: '#c6ecc6'}
-    const baseUrlQuery = 'https://query-gra-hyx2izic7a-uc.a.run.app'
-    const baseUrlDSV = 'https://dsv-hyx2izic7a-uc.a.run.app'
     return  {
         label: 'menu',
         items: [
@@ -11,82 +9,48 @@ export function menuItemsData(newGreed)  {
                 sx: defColor
             },
             {
-                label: 'Clients query',
-                callback: (_, item) => newGreed("https://query-gra-hyx2izic7a-uc.a.run.app/bd/supabase/jsonPG/public.v_persons/ and status <> 'client'", item.label),
-                sx: defColor
-            },
-            {
-                label: 'Soundproofing)',
+                label: 'Jsonplaceholder)', // https://jsonplaceholder.typicode.com/
                 sx: defColor,
-                delay: 300,
                 items: [
                     {
-                        label: 'Today (query)',
-                        callback: (_, item) => newGreed(`${baseUrlQuery}/bd/daas_nma/jsonPG/v_history/ and  date(write_date)=current_date  order by id desc`, item.label),
+                        label: 'users',
+                        callback: (_, item) => newGreed("https://jsonplaceholder.typicode.com/users", item.label),
                         sx: defColor
                     },
                     {
-                        label: 'Month (query)',
-                        callback: (_, item) => newGreed(`${baseUrlQuery}/bd/daas_nma/jsonPG/v_history/ and  date(write_date)>=cast(TO_CHAR(NOW(), 'yyyy-mm-01')as date) order by id desc`, item.label),
+                        label: 'posts',
+                        callback: (_, item) => newGreed("https://jsonplaceholder.typicode.com/posts", item.label),
                         sx: defColor
                     },
                     {
-                        label: 'Month (dsv)',
-                        callback: (_, item) => newGreed(`${baseUrlDSV}/bd/daas_nma/jsonPG/v_history/ and  date(write_date)>=cast(TO_CHAR(NOW(), 'yyyy-mm-01')as date) order by id desc`, item.label),
+                        label: 'comments',
+                        callback: (_, item) => newGreed("https://jsonplaceholder.typicode.com/comments", item.label),
                         sx: defColor
                     },
                     {
-                        label: 'Today  (dsv)',
-                        callback: (_, item) => newGreed(`${baseUrlDSV}/bd/daas_nma/jsonPG/v_history/ and  date(write_date)=current_date  order by id desc`, item.label),
+                        label: 'albums',
+                        callback: (_, item) => newGreed("https://jsonplaceholder.typicode.com/albums", item.label),
                         sx: defColor
                     },
                     {
-                        label: 'Year (working)',
-                        callback: (_, item) => newGreed(`${baseUrlQuery}/bd/daas_nma/jsonPG/v_users_working/   and  date(date_last)>=cast(TO_CHAR(NOW(), 'yyyy-01-01') as date) order by tests_count desc `, item.label),
+                        label: 'photos',
+                        callback: (_, item) => newGreed("https://jsonplaceholder.typicode.com/photos", item.label),
+                        sx: defColor,
+                        disabled: false,
+                    },
+                    {
+                        label: 'todos',
+                        callback: (_, item) => newGreed("https://jsonplaceholder.typicode.com/todos", item.label),
                         sx: defColor,
                         disabled: false,
                     },
                 ],
             },
-            {
-                label: 'Bigquery Google',
-                sx: defColor,
-                items: [
-                    {
-                        label: 'Country',
-                        sx: defColor,
-                        items: [
-                            {
-                                label: 'Country codes',
-                                callback: (_, item) => newGreed(`${baseUrlQuery}/bd/vpn-gra/jsonPG/bigquery-public-data.country_codes.country_codes`, item.label),
-                                sx: defColor,
-                                disabled: false,
-                            },
-                            {
-                                label: 'Country codes start "A"',
-                                callback: (_, item) => newGreed(`${baseUrlQuery}/bd/vpn-gra/jsonPG/bigquery-public-data.country_codes.country_codes / and starts_with(alpha_2_code,'A')`, item.label),
-                                sx: defColor,
-                                disabled: false,
-                            },
-                        ],
-                    },
-                    {
-                        label: 'Type 2',
-                        callback: (event, item) => console.log('Export > FT2 clicked', event, item),
-                        sx: defColor,
-                        disabled: true,
-                    },
-                ],
-            },
+
             {
                 label: 'menu load',
                 callback: () => open('?menu=menu-load.js', "_self"),
                 sx: {color: 'green',bgcolor: '#c6ecc6'},
-            },
-            {
-                label: 'list menu on site',
-                callback: () => open('/save/', ""),
-                sx: {color: 'braun',bgcolor: '#c6ecc6'},
             },
         ],
     }
