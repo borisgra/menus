1d39fed1f192b9f69182d65b7ff2168525bd2ba7
borisgra <boris.gra.dev@gmail.com>
2025-04-14 08:45:15 +0300

    Update menu-host.js

--- menu-host.js
+++ menu-host.js
@@ -1,97 +1,46 @@
 export function menuItemsData(newGreed)  {
     const defColor = {color: '#0033cc',bgcolor: '#c6ecc6'}
-    const baseUrlQuery = 'https://query-gra-hyx2izic7a-uc.a.run.app'
-    const baseUrlDSV = 'https://dsv-hyx2izic7a-uc.a.run.app'
-    // const host = window.location.origin
-    const git_menu_edit = 'https://github.com/borisgra/menus'
+    const host = window.location.origin
+//    console.log(`host=${host}`)
+//     const localUrlQuery = 'http://localhost'
     const git_page_menus = 'https://borisgra.github.io/menus'
-    const git_menu_raw = 'raw.githubusercontent.com/borisgra/menus/refs/heads/main'  // Depricated !
-    const google_menu = 'https://storage.googleapis.com/public-menu'
+    const git_menu_edit = 'https://github.com/borisgra/menus'
     return  {
         label: 'menu',
         items: [
             {
-                label: 'Olympic winners',
+                label: 'Olympic winners (local Query) ',
                 callback: (_, item) => newGreed("https://www.ag-grid.com/example-assets/olympic-winners.json", item.label),
                 sx: defColor
             },
             {
-                label: 'Clients query',
-                callback: (_, item) => newGreed(`${baseUrlQuery}/bd/supabase/jsonPG/public.v_persons/ and status <> 'client'`, item.label),
+                label: 'Persons all ',
+                callback: (_, item) => newGreed(`${host}/bd/QUERY_BD/jsonPG/v_persons`, item.label),
                 sx: defColor
             },
             {
-                label: 'Clients dsv',
-                callback: (_, item) => newGreed(`${baseUrlQuery}/bd/supabase/jsonPG/public.v_persons/ and status <> 'client'`, item.label),
+                label: 'Without Clients',
+                callback: (_, item) => newGreed(`${host}/bd/QUERY_BD/jsonPG/public.v_persons/ and status <> 'client'`, item.label),
                 sx: defColor
             },
             {
-                label: 'Jsonplaceholder)', // https://jsonplaceholder.typicode.com/
-                sx: defColor,
-                items: [
-                    {
-                        label: 'users',
-                        callback: (_, item) => newGreed("https://jsonplaceholder.typicode.com/users", item.label),
-                        sx: defColor
-                    },
-                    {
-                        label: 'posts',
-                        callback: (_, item) => newGreed("https://jsonplaceholder.typicode.com/posts", item.label),
-                        sx: defColor
-                    },
-                    {
-                        label: 'comments',
-                        callback: (_, item) => newGreed("https://jsonplaceholder.typicode.com/comments", item.label),
-                        sx: defColor
-                    },
-                    {
-                        label: 'albums',
-                        callback: (_, item) => newGreed("https://jsonplaceholder.typicode.com/albums", item.label),
-                        sx: defColor
-                    },
-                    {
-                        label: 'photos',
-                        callback: (_, item) => newGreed("https://jsonplaceholder.typicode.com/photos", item.label),
-                        sx: defColor,
-                        disabled: false,
-                    },
-                    {
-                        label: 'todos',
-                        callback: (_, item) => newGreed("https://jsonplaceholder.typicode.com/todos", item.label),
-                        sx: defColor,
-                        disabled: false,
-                    },
-                ],
-            },
-
-            {
-                label: 'Soundproofing)',
+                label: 'Soundproofing',
                 sx: defColor,
                 delay: 300,
                 items: [
                     {
-                        label: 'Today (query)',
-                        callback: (_, item) => newGreed(`${baseUrlQuery}/bd/daas_nma/jsonPG/v_history/ and  date(write_date)=current_date  order by id desc`, item.label),
+                        label: 'Today ',
+                        callback: (_, item) => newGreed(`${host}/bd/daas_nma/jsonPG/v_history/ and  date(write_date)=current_date  order by id desc`, item.label),
                         sx: defColor
                     },
                     {
-                        label: 'Month (query)',
-                        callback: (_, item) => newGreed(`${baseUrlQuery}/bd/daas_nma/jsonPG/v_history/ and  date(write_date)>=cast(TO_CHAR(NOW(), 'yyyy-mm-01')as date) order by id desc`, item.label),
+                        label: 'Month ',
+                        callback: (_, item) => newGreed(`${host}/bd/daas_nma/jsonPG/v_history/ and  date(write_date)>=cast(TO_CHAR(NOW(), 'yyyy-mm-01')as date) order by id desc`, item.label),
                         sx: defColor
                     },
                     {
-                        label: 'Month (dsv)',
-                        callback: (_, item) => newGreed(`${baseUrlDSV}/bd/daas_nma/jsonPG/v_history/ and  date(write_date)>=cast(TO_CHAR(NOW(), 'yyyy-mm-01')as date) order by id desc`, item.label),
-                        sx: defColor
-                    },
-                    {
-                        label: 'Today  (dsv)',
-                        callback: (_, item) => newGreed(`${baseUrlDSV}/bd/daas_nma/jsonPG/v_history/ and  date(write_date)=current_date  order by id desc`, item.label),
-                        sx: defColor
-                    },
-                    {
-                        label: 'Year (working)',
-                        callback: (_, item) => newGreed(`${baseUrl}/bd/daas_nma/jsonPG/v_users_working/   and  date(date_last)>=cast(TO_CHAR(NOW(), 'yyyy-01-01') as date) order by tests_count desc `, item.label),
+                        label: 'Year working ',
+                        callback: (_, item) => newGreed(`${host}/bd/daas_nma/jsonPG/v_users_working/   and  date(date_last)>=cast(TO_CHAR(NOW(), 'yyyy-01-01') as date) order by tests_count desc `, item.label),
                         sx: defColor,
                         disabled: false,
                     },
@@ -107,76 +56,40 @@ export function menuItemsData(newGreed)  {
                         items: [
                             {
                                 label: 'Country codes',
-                                callback: (_, item) => newGreed(`${baseUrlQuery}/bd/vpn-gra/jsonPG/bigquery-public-data.country_codes.country_codes`, item.label),
+                                callback: (_, item) => newGreed(`${host}/bd/vpn-gra/jsonPG/bigquery-public-data.country_codes.country_codes`, item.label),
                                 sx: defColor,
                                 disabled: false,
                             },
                             {
                                 label: 'Country codes start "A"',
-                                callback: (_, item) => newGreed(`${baseUrlQuery}/bd/vpn-gra/jsonPG/bigquery-public-data.country_codes.country_codes / and starts_with(alpha_2_code,'A')`, item.label),
+                                callback: (_, item) => newGreed(`${host}/bd/vpn-gra/jsonPG/bigquery-public-data.country_codes.country_codes / and starts_with(alpha_2_code,'A')`, item.label),
                                 sx: defColor,
                                 disabled: false,
                             },
                         ],
                     },
-                    {
-                        label: 'Type 2',
-                        callback: (event, item) => console.log('Export > FT2 clicked', event, item),
-                        sx: defColor,
-                        disabled: true,
-                    },
                 ],
             },
             {
-                label: 'menu second (local)',
-                callback: () => open('?menu=menu-second.js', "_self"),
-                sx: {color: '#FF0000',bgcolor: '#c6ecc6'},
+                label: 'menu load(start)',
+                callback: () => open(`${host}?menu=/menu-load.js`, "_self"),
+                sx: {color: 'green',bgcolor: '#c6ecc6'},
+            },
+
+            {
+                label: 'Look menu on site',
+                callback: () => open('/menus/menu-host.js', ""),
+                sx: {color: 'braun',bgcolor: '#c6ecc6'},
             },
             {
-                label: `menu-host from github`,
-                callback: () => open(`?menu=${git_page_menus}/menu-host.js`, "_self"),
-                sx: {color: '#FF0000',bgcolor: '#c6ecc6'},
+                label: 'add new menu on site  (flask)',
+                callback: () => open(`/save/?url=https://${git_menu_raw}/menu-git.js`, ""),
+                sx: {color: '#ff66ff',bgcolor: '#c6ecc6'},
             },
             {
-                label: `menu-gra1 from Google Bucket`,
-                callback: () => open(`?menu=${google_menu}/menu-gra1.js`, "_self"),
-                sx: {color: '#FF0000',bgcolor: '#c6ecc6'},
-            },
-            {
-                label: 'Services',
-                sx: defColor,
-                items: [
-                    {
-                        label: 'list menu on site  (flask)',
-                        callback: () => open('/save/', ""),
-                        sx: {color: 'braun', bgcolor: '#c6ecc6'},
-                    },
-                    {
-                        label: 'Look menu on site',
-                        callback: () => open('/menus/menu-git.js', ""),
-                        sx: {color: 'braun',bgcolor: '#c6ecc6'},
-                    },
-                    {
-                        label: 'add new menu on site from git RAW  (flask)',
-                        callback: () => open(`/save/?url=https://${git_menu_raw}/menu-git.js`, ""),
-                        sx: {color: '#e600e6', bgcolor: '#c6ecc6'},
-                    },
-                    {
-                        label: 'edit menu on git',
-                        callback: () => open(git_menu_edit, ""),
-                        sx: {color: '#e600e6', bgcolor: '#c6ecc6'},
-                    },
-                    {
-                        label: 'menu on Google',
-                        callback: () => open(google_menu, ""),
-                        sx: {color: '#b38600', bgcolor: '#c6ecc6'},
-                    },
-                    {
-                        label: 'add new menu on site from Google (flask)',
-                        callback: () => open(`/save/?url=${google_menu}/menu-gra1.js`, ""),
-                        sx: {color: '#b38600', bgcolor: '#c6ecc6'},
-                    },
-                ]
+                label: 'edit menu on git',
+                callback: () => open(git_menu_edit, ""),
+                sx: {color: '#e600e6', bgcolor: '#c6ecc6'},
             },
         ],
     }
