2952d94319c530332b38fdfaf795c4383acc31be
borisgra <boris.gra.dev@gmail.com>
2026-01-13 16:41:23 +0200

    Fix console log and update baseUrlLOCAL handling

--- menu-host.js
+++ menu-host.js
@@ -4,9 +4,9 @@ export function menuItemsData(newGreed)  {
    // const baseUrlLOCAL = 'https://dsv-hyx2izic7a-uc.a.run.app'
    // const baseUrlLOCAL = 'http://boris-gra.xyz:5003'
     const baseUrlLOCAL = window.location.origin  //+'/query'
-    console.log('baseUrlLOCAL='+baseUrlLOCAL
-  //  if (baseUrlLOCAL.search('/query') > -1) 
-  //    baseUrlLOCAL = baseUrlLOCAL+'/query'
+    console.log('baseUrlLOCAL='+baseUrlLOCAL)
+    if (baseUrlLOCAL.search('/query') > -1) 
+      baseUrlLOCAL = baseUrlLOCAL+'/query'
     const baseUrlQuery = 'https://query.boris-gra.xyz'
     // const host = window.location.origin
     const jsonplaceholder = 'https://jsonplaceholder.typicode.com/todos'
